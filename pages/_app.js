d924cb1d01b097934f696aa29b631d9ba18082c7
saeed <saeedkariminia542@gmail.com>
2022-04-07 17:58:48 +0430

    deploy to the vercel

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import "../public/font/fontawesome-pro-5.15.1-web-ulabs/css/all.min.css";
+import "../node_modules/slick-carousel/slick/slick.css";
+import "../node_modules/slick-carousel/slick/slick-theme.css";
+import Layout from "../components/layout/layout";
+import WebappLayout from "../components/webapp-layout/webapp-layout";
+import LoginLayout from "../components/login-layout/logni-layout";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const layouts = {
+    L1: Layout,
+    L2: WebappLayout,
+    L3: LoginLayout,
+};
 
-export default MyApp
+const MyApp = ({ Component, pageProps }) => {
+    const Layout = layouts[Component.layout] || ((children) => <>{children}</>);
+    return (
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+    );
+};
+export default MyApp;
